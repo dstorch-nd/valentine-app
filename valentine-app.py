8547d709f4b9629e51324e0cc0c2400baaceb72a
dstorch-nd <dstorch@nd.edu>
2025-02-02 17:51:43 -0500

    updated code

--- valentine-app.py
+++ valentine-app.py
@@ -9,7 +9,7 @@ def main():
         st.session_state.valentine = False
 
     if not st.session_state.valentine:
-        col1, col2, col3 = st.columns([1.75, 2, 1])
+        col1, col2, col3 = st.columns([1.65, 2, 1])
         with col2:
             st.image("snoopy-hugging-woodstock.jpg", width=150)
         st.markdown("<h1 style='text-align: center;'>Claire, will you be my Valentine?</h1>", unsafe_allow_html=True)
