3eb48ad20fad43b7065a3398bba77a2b14ffdfcf
dstorch-nd <dstorch@nd.edu>
2025-02-02 17:45:59 -0500

    updated code

--- valentine-app.py
+++ valentine-app.py
@@ -9,28 +9,10 @@ def main():
         st.session_state.valentine = False
 
     if not st.session_state.valentine:
-        #st.markdown("<div style='text-align: center;'><img src='snoopy-hugging-woodstock.jpg' width='400'></div>", unsafe_allow_html=True)
-        #st.image("snoopy-hugging-woodstock.jpg", width=400)
-        #st.image("snoopy-hugging-woodstock.jpg", width = 400)
-        #st.title("Claire, will you be my Valentine?")
-        col1, col2, col3 = st.columns([1, 2, 1])  # 2 is the main column width
+        col1, col2, col3 = st.columns([1, 2, 1])
         with col2:
             st.image("snoopy-hugging-woodstock.jpg", width=400)
-        
         st.markdown("<h1 style='text-align: center;'>Claire, will you be my Valentine?</h1>", unsafe_allow_html=True)
-
-        #col = st.columns([1])[0]
-        #with col:
-            # # Increase the button size by applying a style
-            # button_style = """
-            #     <style>
-            #         .stButton button {
-            #             font-size: 24px;
-            #             padding: 20px;
-            #         }
-            #     </style>
-            # """
-            # st.markdown(button_style, unsafe_allow_html=True)
 
         button_style = """
             <style>
@@ -47,7 +29,7 @@ def main():
                 }
             </style>
         """
-        col1, col2, col3 = st.columns([2, 2, 1])  # Middle column is the widest
+        col1, col2, col3 = st.columns([2, 2, 1])
         with col2:
             if st.button("Yes 💖"):
                 st.session_state.valentine = True
@@ -58,27 +40,12 @@ def main():
             
             if no_button:
                 pass
-        # st.markdown(button_style, unsafe_allow_html=True)
-
-        #     if st.button("Yes 💖"):
-        #         st.session_state.valentine = True
-        #         st.rerun()
-        
-        #     no_x, no_y = st.session_state.no_button_position
-        #     no_button = st.button("No ❌", key="no")
-            
-        #     if no_button:
-        #         pass
 
     else:
-        #st.markdown("<div style='text-align: center;'><img src='snoopy-cheering.jpg' width='500'></div>", unsafe_allow_html=True)
-        col1, col2, col3 = st.columns([1, 2, 1])  # 2 is the main column width
+        col1, col2, col3 = st.columns([1, 2, 1])
         with col2:
             st.image("snoopy-cheering.jpg", width = 500)
-        # st.image("snoopy-cheering.jpg", width = 500)
         st.markdown("<h1 style='text-align: center;'>Yay! Happy Valentine's Day! 💕</h1>", unsafe_allow_html=True)
-        #st.title("Yay! Happy Valentine's Day! 💕")
-        #st.write("You made the right choice! 😊")
         st.markdown("<h2 style='text-align: center;'>T-Minus two weeks until I see you!</h2>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
